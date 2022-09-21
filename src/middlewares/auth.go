89717b6581d0115359f8fc82bd00206674507e96
Scalia Abel <bidayuhcoder@protonmail.com>
2022-09-21 21:34:02 +0800

    feat: add auth middleware

---
+++ src/middlewares/auth.go
@@ -0,0 +1,44 @@
+package middlewares
+
+import (
+	"ambassador/src/config"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
+)
+
+func IsAuthenticated(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(config.Config("JWT_SECRET")), nil
+	})
+
+	if err != nil || !token.Valid {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated!",
+		})
+	}
+
+	return c.Next()
+}
+
+func GetUserId(c *fiber.Ctx) (uint, error) {
+	cookie := c.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(config.Config("JWT_SECRET")), nil
+	})
+
+	if err != nil || !token.Valid {
+		return 0, err
+	}
+
+	payload := token.Claims.(*jwt.StandardClaims)
+
+	id, _ := strconv.Atoi(payload.Subject)
+
+	return uint(id), nil
+}

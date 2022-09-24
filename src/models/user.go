ee20c4fada9e243c62d37dc54d2db45ceee29af2
Scalia Abel <bidayuhcoder@protonmail.com>
2022-09-25 06:33:31 +0800

    refactor: add aliases and calculateRevenue function

--- src/models/user.go
+++ src/models/user.go
@@ -1,13 +1,17 @@
 package models
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	Model
-	Name         string `json:"name"`
-	Email        string `json:"email" gorm:"unique"`
-	Password     []byte `json:"-"`
-	IsAmbassador bool   `json:"-"`
+	Name         string   `json:"name"`
+	Email        string   `json:"email" gorm:"unique"`
+	Password     []byte   `json:"-"`
+	IsAmbassador bool     `json:"-"`
+	Revenue      *float64 `json:"revenue,omitempty" gorm:"-"`
 }
 
 func (user *User) SetPassword(password string) {
@@ -18,4 +22,46 @@ func (user *User) SetPassword(password string) {
 
 func (user *User) CheckPassword(password string) error {
 	return bcrypt.CompareHashAndPassword(user.Password, []byte(password))
+}
+
+type Admin User
+
+func (admin *Admin) CalculateRevenue(db *gorm.DB) {
+	var orders []Order
+
+	db.Preload("OrderItems").Find(&orders, &Order{
+		UserId:   admin.Id,
+		Complete: true,
+	})
+
+	var revenue float64 = 0
+
+	for _, order := range orders {
+		for _, orderItem := range order.OrderItems {
+			revenue += orderItem.AdminRevenue
+		}
+	}
+
+	admin.Revenue = &revenue
+}
+
+type Ambassador User
+
+func (ambassador *Ambassador) CalculateRevenue(db *gorm.DB) {
+	var orders []Order
+
+	db.Preload("OrderItems").Find(&orders, &Order{
+		UserId:   ambassador.Id,
+		Complete: true,
+	})
+
+	var revenue float64 = 0
+
+	for _, order := range orders {
+		for _, orderItem := range order.OrderItems {
+			revenue += orderItem.AmbassadorRevenue
+		}
+	}
+
+	ambassador.Revenue = &revenue
 }

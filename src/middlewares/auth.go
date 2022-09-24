28d6d4bd7dd7ac3cc66e3522abddf56c3c5c194e
Scalia Abel <bidayuhcoder@protonmail.com>
2022-09-24 21:18:58 +0800

    feat: update jwt to use scopes

--- src/middlewares/auth.go
+++ src/middlewares/auth.go
@@ -3,17 +3,20 @@ package middlewares
 import (
 	"ambassador/src/config"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt"
 )
 
-func IsAuthenticated(c *fiber.Ctx) error {
-	cookie := c.Cookies("jwt")
+type ClaimsWithScope struct {
+	jwt.StandardClaims
+	Scope string
+}
 
-	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
-		return []byte(config.Config("JWT_SECRET")), nil
-	})
+func IsAuthenticated(c *fiber.Ctx) error {
+	token, err := GetCookieToken(c)
 
 	if err != nil || !token.Valid {
 		c.Status(fiber.StatusUnauthorized)
@@ -22,21 +25,45 @@ func IsAuthenticated(c *fiber.Ctx) error {
 		})
 	}
 
+	payload := token.Claims.(*ClaimsWithScope)
+	isAmbassador := strings.Contains(c.Path(), "/api/ambassador")
+
+	if (payload.Scope == "admin" && isAmbassador) || (payload.Scope == "ambassador" && !isAmbassador) {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthorized!",
+		})
+	}
+
 	return c.Next()
 }
 
-func GetUserId(c *fiber.Ctx) (uint, error) {
+func GenerateJWT(id uint, scope string) (string, error) {
+	payload := ClaimsWithScope{}
+
+	payload.Subject = strconv.Itoa(int(id))
+	payload.ExpiresAt = time.Now().Add(time.Hour * 24).Unix()
+	payload.Scope = scope
+
+	return jwt.NewWithClaims(jwt.SigningMethodHS256, payload).SignedString([]byte(config.Config("JWT_SECRET")))
+}
+
+func GetCookieToken(c *fiber.Ctx) (*jwt.Token, error) {
 	cookie := c.Cookies("jwt")
 
-	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+	return jwt.ParseWithClaims(cookie, &ClaimsWithScope{}, func(t *jwt.Token) (interface{}, error) {
 		return []byte(config.Config("JWT_SECRET")), nil
 	})
+}
+
+func GetUserId(c *fiber.Ctx) (uint, error) {
+	token, err := GetCookieToken(c)
 
 	if err != nil || !token.Valid {
 		return 0, err
 	}
 
-	payload := token.Claims.(*jwt.StandardClaims)
+	payload := token.Claims.(*ClaimsWithScope)
 
 	id, _ := strconv.Atoi(payload.Subject)
 

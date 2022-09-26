3c7359540666821950bb9ff9c7e6c81fa8c3a55c
Scalia Abel <bidayuhcoder@protonmail.com>
2022-09-26 22:24:05 +0800

    feat: add setup cache channel

--- src/database/redis.go
+++ src/database/redis.go
@@ -1,12 +1,50 @@
 package database
 
-import "github.com/go-redis/redis/v9"
+import (
+	"context"
+	"time"
+
+	"github.com/go-redis/redis/v9"
+)
 
 var Cache *redis.Client
+var CacheChannel chan string
 
 func SetupRedis() {
 	Cache = redis.NewClient(&redis.Options{
 		Addr: "redis:6379",
 		DB:   0,
 	})
+}
+
+func SetupCacheChannel() {
+	CacheChannel = make(chan string)
+
+	go func(ch chan string) {
+		for {
+			time.Sleep(3 * time.Second)
+
+			Cache.Del(context.Background(), <-ch)
+		}
+	}(CacheChannel)
+}
+
+func ClearCache(keys ...string) {
+	for _, key := range keys {
+		CacheChannel <- key
+	}
+}
+
+func SetCache(key string, value []byte, ttl time.Duration) error {
+	return Cache.Set(context.Background(), key, value, ttl).Err()
+}
+
+func GetCache(key string) (string, error) {
+	result, err := Cache.Get(context.Background(), key).Result()
+
+	if err != nil {
+		return "", err
+	}
+
+	return result, nil
 }
